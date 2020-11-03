c39905d86a77fde309d919031a4e577bc2c143b5
Sunil pargi <sunilpargi121@gmail.com>
2020-11-03 20:31:32 +0530

    added player attack animation

---
+++ 3D Fight Arena/Assets/Script/Player/PlayerAttackInput.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackInput : MonoBehaviour
+{
+    private CharacterAnimations playerAnimation;
+
+    public GameObject attackPoint;
+
+    
+
+    void Awake()
+    {
+        playerAnimation = GetComponent<CharacterAnimations>();
+       
+    }
+
+    void Update()
+    {
+
+        // defend when J pressed DOWN
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+
+            playerAnimation.Defend(true);
+
+           
+
+        }
+
+        // release Defence when J Is released
+        if (Input.GetKeyUp(KeyCode.J))
+        {
+
+            playerAnimation.UnFreezeAnimation();
+            playerAnimation.Defend(false);
+
+           
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+
+            if (Random.Range(0, 2) > 0)
+            {
+
+                playerAnimation.Attack_1();
+
+               
+
+            }
+            else
+            {
+
+                playerAnimation.Attack_2();
+
+             
+
+            }
+
+        }
+
+    }
+}
